9cc465f0ce55096788616b27554340a686e6c3c1
Naveenmallada9090 <malladanaveen@gmail.com>
2026-05-10 12:15:43 +0530

    remove

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const cors = require("cors");
 const dotenv = require('dotenv');
 dotenv.config();
 const rootRouter = require("./routes/index");
 
 const app = express();
 
-const corsOptions = {
-  origin: process.env.FRONTEND_URL || '*',
-  credentials: true,
-};
-app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
