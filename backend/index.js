a5501e4473e51db6ddc953faf05782cebf7ab009
Naveenmallada9090 <malladanaveen@gmail.com>
2026-05-12 12:40:47 +0530

    doner

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 const rootRouter = require("./routes/index");
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
