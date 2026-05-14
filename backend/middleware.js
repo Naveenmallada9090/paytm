47f6b407e11a7eefbcc93bd809cf3beedc6edc15
Naveenmallada9090 <malladanaveen@gmail.com>
2026-05-14 17:56:20 +0530

    done changes in jwt

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,5 +1,5 @@
-const JWT_SECRET = require("./config");
 const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
